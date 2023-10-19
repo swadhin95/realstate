0fe427769c9c231b39d2f3fefe4b0a01e0b3ef9c
swadhin95 <swa.ctg@gmail.com>
2023-10-19 11:39:41 +0600

    Admin Panel Setup , Profile , Image, password update
    Admin Panel Setup , Profile , Image, password update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Contracts\Service\Attribute\Required;
 
 class AdminController extends Controller
 {
@@ -24,5 +27,84 @@ class AdminController extends Controller
 
     public function AdminLogin(){
         return view('admin.admin_login');
+    }
+
+    public function AdminProfile(){
+
+        $id = Auth::user()->id;
+        $profileData= User::find($id);
+
+        return view('admin.admin_profile_view',compact('profileData'));
+
+    }
+
+    public function AdminProfileStore(Request $request){
+        $id = Auth::user()->id;
+        $data= User::find($id);
+
+        $data->username = $request->username;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+        if($request->file('photo')){
+            $file = $request->file('photo');
+            @unlink(public_path('upload/admin_images/'.$data->photo));
+            // @unlink(public_path('upload/admin_images'.$data->photo));
+            $fileName= date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'),$fileName);
+            $data['photo']= $fileName;
+        }
+
+        $data->save();
+
+        $notification = array(
+            'message' => 'Admin Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+
+
+    }
+
+    public function AdminChangePassword(){
+
+        $id = Auth::user()->id;
+        $profileData= User::find($id);
+
+        return view('admin.admin_change_password',compact('profileData'));
+        
+    }
+
+    public function AdminPasswordUpdate(Request $request){
+
+        //Validate
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed'
+        ]);
+
+        /// Match The Old Password
+        if(!Hash::check($request->old_password,Auth::user()->password)){
+            $notification = array(
+                'message' => 'Old Password is not matched',
+                'alert-type' => 'error'
+            );
+
+            return back()->with($notification);
+        }
+
+        ///Update New Password
+        User::whereId(Auth::user()->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+
+        $notification = array(
+            'message' => 'Password Updated',
+            'alert-type' => 'success'
+        );
+        return back()->with($notification);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,10 @@ require __DIR__.'/auth.php';
 Route::middleware(['auth','role:admin'])->group(function(){
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
     Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+    Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+    Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])->name('admin.change.password');
+    Route::post('/admin/password/update', [AdminController::class, 'AdminPasswordUpdate'])->name('admin.password.update');
 });//End of Admin Middleware
 
 
