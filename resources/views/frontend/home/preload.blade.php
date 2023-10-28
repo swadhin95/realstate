b45801ca6e92e354d5ca6ec2b523c6359a5e59b1
swadhin95 <swa.ctg@gmail.com>
2023-10-28 14:05:03 +0600

    Frontend Template setup and Login Registration complete
    Frontend Template setup and Login Registration complete

---
+++ resources/views/frontend/home/preload.blade.php
@@ -0,0 +1,70 @@
+<div class="loader-wrap">
+    <div class="preloader">
+        <div class="preloader-close"><i class="far fa-times"></i></div>
+        <div id="handle-preloader" class="handle-preloader">
+            <div class="animation-preloader">
+                <div class="spinner"></div>
+                <div class="txt-loading">
+                    <span data-text-preloader="S" class="letters-loading">
+                       S
+                    </span>
+                    <span data-text-preloader="W" class="letters-loading">
+                        W
+                    </span>
+                    <span data-text-preloader="A" class="letters-loading">
+                        A
+                    </span>
+                    <span data-text-preloader="D" class="letters-loading">
+                        D
+                    </span>
+                    <span data-text-preloader="H" class="letters-loading">
+                       H
+                    </span>
+                    <span data-text-preloader="I" class="letters-loading">
+                        I
+                    </span>
+                    <span data-text-preloader="N" class="letters-loading">
+                        N
+                    </span>
+                    <span data-text-preloader=" " class="letters-loading">
+                          
+                    </span>
+                    <span data-text-preloader=" " class="letters-loading">
+                          
+                    </span>
+                    <span data-text-preloader="R" class="letters-loading">
+                        R
+                    </span>
+                    
+                    <span data-text-preloader="E" class="letters-loading">
+                        E
+                    </span>
+                    <span data-text-preloader="A" class="letters-loading">
+                        A
+                    </span>
+                    <span data-text-preloader="L" class="letters-loading">
+                        L
+                    </span>
+                    <span data-text-preloader=" " class="letters-loading">
+                          
+                    </span>
+                    <span data-text-preloader="S" class="letters-loading">
+                        S
+                    </span>
+                    <span data-text-preloader="T" class="letters-loading">
+                        T
+                    </span>
+                    <span data-text-preloader="A" class="letters-loading">
+                        A
+                    </span>
+                    <span data-text-preloader="T" class="letters-loading">
+                        T
+                    </span>
+                    <span data-text-preloader="E" class="letters-loading">
+                        E
+                    </span>
+                </div>
+            </div>  
+        </div>
+    </div>
+</div>
